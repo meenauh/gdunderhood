4cec39e25eebf1cfe1e484556efced6bb5be00f7
meenauh <meenauh@gmail.com>
2016-07-10 08:02:40 +0100

    once again

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "set git environment"
 git config user.email "iamstarkov+tachikombie@gmail.com"
 git config user.name "Tachikoma Zombie"
 git remote rm origin
-git remote add origin https://meenauh:${GITHUB_TOKEN}@github.com/meenauh/https://github.com/meenauh/gdunderhood.git
+git remote add origin https://meenauh:${GITHUB_TOKEN}@github.com/meenauh/meenauh/gdunderhood.git
 git checkout master
 
 echo "run update"
