287c5fd1f43f6ce71635519a39926bf0b488d91e
Vladimir Starkov <iamstarkov@gmail.com>
2016-07-21 01:16:49 +0200

    make eslint happy

--- authors.js
+++ authors.js
@@ -2,5 +2,5 @@
 
 export default [
   // { username: 'VKSpaz',  start: '11 July 2016', first: '752361980195995648', post: false },
-  { username: 'vedmich',  start: '04 July 2016', first: '749880962457141248' }
+  { username: 'vedmich',  start: '04 July 2016', first: '749880962457141248' },
 ];
