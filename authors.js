d706dd3ef8e3b118d6a68db1d0f9b9b27962862f
Vladimir Starkov <iamstarkov@gmail.com>
2016-07-21 01:26:10 +0200

    add @YarKravtsov

--- authors.js
+++ authors.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  { username: 'VKSpaz',   start: '11 July 2016', first: '752361980195995648', post: false },
-  { username: 'vedmich',  start: '04 July 2016', first: '749880962457141248' },
+  // post: false
+  { username: 'YarKravtsov',   start: '18 July 2016', first: '754938140444549120', post: false },
+  { username: 'VKSpaz',        start: '11 July 2016', first: '752361980195995648' },
+  { username: 'vedmich',       start: '04 July 2016', first: '749880962457141248' },
 ];
