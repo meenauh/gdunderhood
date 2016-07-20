b650052d1fad65ef4969b1a1276d3c0d8e1aa329
Vladimir Starkov <iamstarkov@gmail.com>
2016-07-21 01:17:56 +0200

    add @VKSpaz

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  // { username: 'VKSpaz',  start: '11 July 2016', first: '752361980195995648', post: false },
+  { username: 'VKSpaz',   start: '11 July 2016', first: '752361980195995648', post: false },
   { username: 'vedmich',  start: '04 July 2016', first: '749880962457141248' },
 ];
