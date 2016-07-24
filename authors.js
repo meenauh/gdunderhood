df18742c64a58563c17d35a0d888a8b6ba0ebc2c
Ilya <meenauh@gmail.com>
2016-07-25 01:47:42 +0500

    fixing vedmich first tweet

--- authors.js
+++ authors.js
@@ -4,5 +4,5 @@ export default [
   // post: false
   { username: 'YarKravtsov',   start: '18 July 2016', first: '754938140444549120' },
   { username: 'VKSpaz',        start: '11 July 2016', first: '752361980195995648' },
-  { username: 'vedmich',       start: '04 July 2016', first: '749880962457141248' },
+  { username: 'vedmich',       start: '04 July 2016', first: '749883140706037761' },
 ];
