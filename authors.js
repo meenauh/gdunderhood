47d6b94afe9f43ab7e1c7f7a9a769c33b7dd2e7c
Ilya <meenauh@gmail.com>
2016-07-25 01:35:22 +0500

    finish YarKravtsov week

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@
 
 export default [
   // post: false
-  { username: 'YarKravtsov',   start: '18 July 2016', first: '754938140444549120', post: false },
+  { username: 'YarKravtsov',   start: '18 July 2016', first: '754938140444549120' },
   { username: 'VKSpaz',        start: '11 July 2016', first: '752361980195995648' },
   { username: 'vedmich',       start: '04 July 2016', first: '749880962457141248' },
 ];
