4480714dc11dd2bd5c856819c0b5238440282eaf
Ilya <meenauh@gmail.com>
2016-08-01 14:14:02 +0500

    adding @deimosalife, changing month format

--- authors.js
+++ authors.js
@@ -2,8 +2,9 @@
 
 export default [
   // post: false
-  { username: 'retropcan',     start: '25 July 2016', first: '757478312360968192' },
-  { username: 'YarKravtsov',   start: '18 July 2016', first: '754938140444549120' },
-  { username: 'VKSpaz',        start: '11 July 2016', first: '752361980195995648' },
-  { username: 'vedmich',       start: '04 July 2016', first: '749883140706037761' }
+  { username: 'deimosalife',   start: '01 Aug 2016', first: '760036334581014528', post: false },
+  { username: 'retropcan',     start: '25 Jul 2016', first: '757478312360968192' },
+  { username: 'YarKravtsov',   start: '18 Jul 2016', first: '754938140444549120' },
+  { username: 'VKSpaz',        start: '11 Jul 2016', first: '752361980195995648' },
+  { username: 'vedmich',       start: '04 Jul 2016', first: '749883140706037761' }
 ];
