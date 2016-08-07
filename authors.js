7de23c5fe1b67def47ad18950a3ec7e94b837b99
Ilya Ostashko <meenauh@gmail.com>
2016-08-08 02:58:50 +0500

    finish deimosalife week

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@
 
 export default [
   // post: false
-  { username: 'deimosalife',   start: '01 Aug 2016', first: '760036334581014528', post: false },
+  { username: 'deimosalife',   start: '01 Aug 2016', first: '760036334581014528' },
   { username: 'retropcan',     start: '25 Jul 2016', first: '757478312360968192' },
   { username: 'YarKravtsov',   start: '18 Jul 2016', first: '754938140444549120' },
   { username: 'VKSpaz',        start: '11 Jul 2016', first: '752361980195995648' },
