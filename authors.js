82792a8fb12e94c5cfbfb3031c04bed0e6e83eba
Ilya Ostashko <meenauh@gmail.com>
2016-08-15 17:41:39 +0500

    starting john_grey_grey week

--- authors.js
+++ authors.js
@@ -2,10 +2,11 @@
 
 export default [
   // post: false
-  { username: 'meenauh',       start: '08 Aug 2016', first: '762535730749005824' },
-  { username: 'deimosalife',   start: '01 Aug 2016', first: '760036334581014528' },
-  { username: 'retropcan',     start: '25 Jul 2016', first: '757478312360968192' },
-  { username: 'YarKravtsov',   start: '18 Jul 2016', first: '754938140444549120' },
-  { username: 'VKSpaz',        start: '11 Jul 2016', first: '752361980195995648' },
-  { username: 'vedmich',       start: '04 Jul 2016', first: '749883140706037761' }
+  { username: 'john_grey_grey', start: '15 Aug 2016', first: '765112568004349952', post: false },
+  { username: 'meenauh',        start: '08 Aug 2016', first: '762535730749005824' },
+  { username: 'deimosalife',    start: '01 Aug 2016', first: '760036334581014528' },
+  { username: 'retropcan',      start: '25 Jul 2016', first: '757478312360968192' },
+  { username: 'YarKravtsov',    start: '18 Jul 2016', first: '754938140444549120' },
+  { username: 'VKSpaz',         start: '11 Jul 2016', first: '752361980195995648' },
+  { username: 'vedmich',        start: '04 Jul 2016', first: '749883140706037761' }
 ];
