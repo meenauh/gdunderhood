1c40baf9087ba1177a2396037b435f5024642f86
Ilya Ostashko <meenauh@gmail.com>
2016-08-22 09:00:38 +0200

    finishing John Grey week

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@
 
 export default [
   // post: false
-  { username: 'john_grey_grey', start: '15 Aug 2016', first: '765112568004349952', post: false },
+  { username: 'john_grey_grey', start: '15 Aug 2016', first: '765112568004349952' },
   { username: 'meenauh',        start: '08 Aug 2016', first: '762535730749005824' },
   { username: 'deimosalife',    start: '01 Aug 2016', first: '760036334581014528' },
   { username: 'retropcan',      start: '25 Jul 2016', first: '757478312360968192' },
