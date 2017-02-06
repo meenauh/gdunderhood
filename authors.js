6e2797b714bcf09df83e3cc8d911d7b5dd247d6f
Ilya Ostashko <meenauh@gmail.com>
2017-02-06 16:41:07 +0500

    start MediocreHenrik week

--- authors.js
+++ authors.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-  // , post: false
+  // , post: false 
+  { username: 'MediocreHenrik', start: '06 Feb 2017', first: '828559837827100672', post: false },
   { username: 'MuchGamesWow',   start: '30 Jan 2017', first: '825970599898001410' },
   { username: 'cateIIo',        start: '23 Jan 2017', first: '823447064533200899' },
   { username: 'aaafrancisc',    start: '16 Jan 2017', first: '820914446725971968' },
