f6ff5b6c79dc4bdeb0af7177fbd4ac3c4b2c8f28
meenauh <meenauh@gmail.com>
2017-05-11 00:13:43 +0200

    start StasShostak week

--- authors.js
+++ authors.js
@@ -2,6 +2,7 @@
 
 export default [
   // , post: false
+  { username: 'StasShostak.',   start: '08 May 2017', first: '861495417594413056', post: false},
   { username: 'diforus',        start: '01 May 2017', first: '858929636729733120'},
   { username: 'iluf1',          start: '24 Apr 2017', first: '856403571121635328' },
   { username: 'TheAllThing',    start: '17 Apr 2017', first: '853893175517249536' },
