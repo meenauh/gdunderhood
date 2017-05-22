408ccb2406892be352e5850ff0e7edbe8a91b101
Ilya Ostashko <meenauh@gmail.com>
2017-05-22 12:18:36 +0200

    end Die_With_Glory week

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@
 
 export default [
   // , post: false
-  { username: 'Die_With_Glory', start: '15 May 2017', first: '864087225264472064', post: false },
+  { username: 'Die_With_Glory', start: '15 May 2017', first: '864087225264472064' },
   { username: 'StasShostak.',   start: '08 May 2017', first: '861495417594413056' },
   { username: 'diforus',        start: '01 May 2017', first: '858929636729733120' },
   { username: 'iluf1',          start: '24 Apr 2017', first: '856403571121635328' },
