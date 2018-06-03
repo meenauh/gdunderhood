2cf41ddb620e6adf9b54840fee544fe2fab80071
Ilya Ostashko <meenauh@gmail.com>
2018-06-03 23:21:44 +0500

    end vgf365 week

--- authors.js
+++ authors.js
@@ -2,7 +2,7 @@
 
 export default [
   // , post: false 
-  { username: 'vgf365',           start: '28 May 2018', first: '1000995336171614209', post: false  },
+  { username: 'vgf365',           start: '28 May 2018', first: '1000995336171614209' },
   { username: 'hickoma',          start: '07 May 2018', first: '993411241711456262' },
   { username: 'ovca_snob',        start: '05 Feb 2018', first: '960389135167930368' },
   { username: 'YarKravtsov_2',    start: '15 Jan 2018', first: '952826394077220864' },
